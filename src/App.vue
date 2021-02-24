e1db64a03f8e030ffa0ba7b4c7b9ecff76b66d72
fatkhanm <fatkhan.masruri45@gmail.com>
2021-02-24 22:31:51 +0700

    gabungin semua canvas

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,45 @@
 <div class = "container">
 <h3>Mari Menggambar</h3>
   <div class = "content-container">
-    <div class = "content">
-    <h3>Kontol</h3>
-    </div>
+    <input type="radio" id="garis" value="1" v-model="draw">
+    <label for="garis">Garis</label>
+    <input type="radio" id="persegi" value="2" v-model="draw">
+    <label for="persegi">Persegi</label>
+    <input type="radio" id="poligon" value="3" v-model="draw">
+    <label for="poligon">Poligon</label>
   </div>
-  <div class = "canva-container">
-    <Persegi />
+  <div>
+    <template v-if = "draw === '1'">
+      <h3>Halo Garis</h3>
+    </template>
+    <template v-else-if = "draw === '2'">
+      <Persegi />
+    </template>
+    <template v-else-if = "draw === '3'">
+      <h3>Halo Poligon</h3>
+    </template>
   </div>
 </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue';
+import { defineComponent, onMounted,onUpdated,ref } from 'vue';
 import Persegi from './components/Persegi.vue';
+import VueRouter from 'vue-router';
+
 
 export default defineComponent({
   name: 'App',
-  components: {
-    Persegi,
-  },
+  components:{Persegi},
   setup() {
+    const draw = ref('');
     onMounted(() => {
       console.log('App mounted');
     });
+    onUpdated(()=>{
+      console.log(draw);
+    });
+    return {draw};
   },
 });
 </script>
