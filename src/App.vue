3330b038f9e1cf2f9ae2a71311954f747314967e
Samuel <13518041@std.stei.itb.ac.id>
2021-02-24 22:58:37 +0700

    Resolve conflicts

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,21 @@
     </div>
     <div>
       <template v-if="draw === '1'">
+<<<<<<< HEAD
         <Canvas />
+=======
+        <h3>Halo Garis</h3>
+>>>>>>> c8bf1fe (Fix linting)
       </template>
       <template v-else-if="draw === '2'">
         <Persegi />
       </template>
       <template v-else-if="draw === '3'">
+<<<<<<< HEAD
         <Poligon />
+=======
+        <h3>Halo Poligon</h3>
+>>>>>>> c8bf1fe (Fix linting)
       </template>
     </div>
   </div>
@@ -26,6 +34,7 @@
 <script lang="ts">
 import { defineComponent, onMounted, onUpdated, ref } from 'vue';
 import Persegi from './components/Persegi.vue';
+<<<<<<< HEAD
 import Canvas from './components/Canvas.vue';
 import Poligon from './components/Poligon.vue';
 
@@ -36,6 +45,12 @@ export default defineComponent({
     Canvas,
     Poligon,
   },
+=======
+
+export default defineComponent({
+  name: 'App',
+  components: { Persegi },
+>>>>>>> c8bf1fe (Fix linting)
   setup() {
     const draw = ref('');
     onMounted(() => {
