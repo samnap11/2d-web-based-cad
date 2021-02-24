c8bf1feaaaf11753e234c7fe15740dc451226e51
Samuel <13518041@std.stei.itb.ac.id>
2021-02-24 22:58:37 +0700

    Fix linting

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,44 @@
 <template>
-<div class = "container">
-<h3>Mari Menggambar</h3>
-  <div class = "content-container">
-    <input type="radio" id="garis" value="1" v-model="draw">
-    <label for="garis">Garis</label>
-    <input type="radio" id="persegi" value="2" v-model="draw">
-    <label for="persegi">Persegi</label>
-    <input type="radio" id="poligon" value="3" v-model="draw">
-    <label for="poligon">Poligon</label>
+  <div class="container">
+    <h3>Mari Menggambar</h3>
+    <div class="content-container">
+      <input type="radio" id="garis" value="1" v-model="draw" />
+      <label for="garis">Garis</label>
+      <input type="radio" id="persegi" value="2" v-model="draw" />
+      <label for="persegi">Persegi</label>
+      <input type="radio" id="poligon" value="3" v-model="draw" />
+      <label for="poligon">Poligon</label>
+    </div>
+    <div>
+      <template v-if="draw === '1'">
+        <h3>Halo Garis</h3>
+      </template>
+      <template v-else-if="draw === '2'">
+        <Persegi />
+      </template>
+      <template v-else-if="draw === '3'">
+        <h3>Halo Poligon</h3>
+      </template>
+    </div>
   </div>
-  <div>
-    <template v-if = "draw === '1'">
-      <h3>Halo Garis</h3>
-    </template>
-    <template v-else-if = "draw === '2'">
-      <Persegi />
-    </template>
-    <template v-else-if = "draw === '3'">
-      <h3>Halo Poligon</h3>
-    </template>
-  </div>
-</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted,onUpdated,ref } from 'vue';
+import { defineComponent, onMounted, onUpdated, ref } from 'vue';
 import Persegi from './components/Persegi.vue';
-import VueRouter from 'vue-router';
-
 
 export default defineComponent({
   name: 'App',
-  components:{Persegi},
+  components: { Persegi },
   setup() {
     const draw = ref('');
     onMounted(() => {
       console.log('App mounted');
     });
-    onUpdated(()=>{
+    onUpdated(() => {
       console.log(draw);
     });
-    return {draw};
+    return { draw };
   },
 });
 </script>
